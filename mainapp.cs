869607fb8139b2db224deddaaabdf09bcc69a988
reallynull <67848586+reallynull@users.noreply.github.com>
2020-07-06 20:06:17 -0400

    Create mainapp.cs

---
+++ mainapp.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace RareWasayApp { 
+    class Title { 
+       static void Main(string[] args) { 
+          Console.WriteLine("RareWasayWorld");
+          Console.ReadKey();}
+   /* rarewasay app display */
+     class Display { 
+         string embed = "https://www.youtube-nocookie.com/embed/FKQ7DggATXE";
+            WriteLine embed.ReadKey1();
+            private WriteLine Getembed.ReadKey => Embed.ReadKey1;
+            public string Embed { get => embed; set => embed = value; }
+        }
+    }}
