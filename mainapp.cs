7c41c0706f5270a5b3cd56f7d4907ff61bddd1db
reallynull <67848586+reallynull@users.noreply.github.com>
2020-07-06 20:44:25 -0400

    Update mainapp.cs

--- mainapp.cs
+++ mainapp.cs
@@ -1,12 +1,11 @@
 using System;
 
 namespace RareWasayApp { 
-    class Title { 
+    class Wasay { 
        static void Main(string[] args) { 
           Console.WriteLine("RareWasayWorld");
           Console.ReadKey();}
-   /* rarewasay app display */
-     class Display { 
+   class Display { 
          string embed = "https://www.youtube-nocookie.com/embed/FKQ7DggATXE";
             WriteLine embed.ReadKey1();
             private WriteLine Getembed.ReadKey => Embed.ReadKey1;
