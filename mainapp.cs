937eeff511e5f19bf154b6ef0cc152565383a907
grubwuf <66730590+grubwuf@users.noreply.github.com>
2020-07-12 18:31:00 +0200

    NO LONGER UGLY

--- mainapp.cs
+++ mainapp.cs
@@ -1,14 +1,26 @@
 using System;
 
-namespace RareWasayApp { 
-    class Wasay { 
-       static void Main(string[] args) { 
-          Console.WriteLine("RareWasayWorld");
-          Console.ReadKey();}
-   class Display { 
-         string embed = "https://www.youtube-nocookie.com/embed/FKQ7DggATXE";
+namespace RareWasayApp
+{
+    class Wasay
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("RareWasayWorld");
+            Console.ReadKey();
+        }
+
+        class Display
+        { 
+            string embed = "https://www.youtube-nocookie.com/embed/FKQ7DggATXE";
             WriteLine embed.ReadKey1();
             private WriteLine Getembed.ReadKey => Embed.ReadKey1;
-            public string Embed { get => embed; set => embed = value; }
+
+            public string Embed
+            {
+                get => embed;
+                set => embed = value;
+            }
         }
-    }}
+    }
+}
